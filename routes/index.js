75f7efaadd2ab54ecbfb31db91135ab61fd5a72f
quhuohuo <qh_work@outlook.com>
2017-05-23 00:21:17 +0800

    first commit
    first commit
    讨论数据库、页面、已经要用的框架，复习redux，阅读react-bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./views/index.html');
+  res.render('index.ejs');
 });
+
+
 
 module.exports = router;
