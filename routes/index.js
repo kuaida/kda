393836d6493d191f74bd869d4e91bea57b058b08
jindw <jindw@outlook.com>
2017-05-23 11:25:44 +0800

    保证routes/index.js的一致性

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.sendfile('./views/index.html');
-  res.render('index');
+  res.render('index',{user:''});
 });
 
 router.get('/userinfo',function(req,res,next){
